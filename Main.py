f502968d2cd2965374066d4e95160d158578e5be
John Seidl <jseidl2@wgu.edu>
2025-03-12 16:22:21 -0500

    Creating a class to rep columns in table, creating database and session to interact with database.

--- Main.py
+++ Main.py
@@ -12,7 +12,39 @@ day = int(input("Enter Day: "))
 years = input("Enter years: ").split()
 
 
-# C3 - Creating an instance of the WeatherData class
+# Creating an instance of the WeatherData class
 weather_info = WeatherData(latitude, longitude, month, day, years)
 annual_weather = weather_info.get_weather()
 #print(annual_weather)
+
+base = declarative_base()
+
+# Creating a table using SQLite and SQLAlchemy
+
+#Creating second class with attributes that represent columns in the SQLite table
+class WeatherTable(base):
+    __tablename__ = 'Weather'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    month = Column(Integer, nullable=False)
+    day = Column(Integer, nullable=False)
+    years = Column(Integer, nullable=False)
+    mean_temp = Column(Float)
+    min_temp = Column(Float)
+    max_temp = Column(Float)
+    mean_wind = Column(Float)
+    min_wind = Column(Float)
+    max_wind = Column(Float)
+    total_precipitation = Column(Float)
+    min_precipitation = Column(Float)
+    max_precipitation = Column(Float)
+
+#Creating the database
+engine = create_engine('sqlite:///weather.db')
+base.metadata.drop_all(engine)
+base.metadata.create_all(engine)
+
+#Creating a session to interact with data and database
+Session = sessionmaker(bind=engine)
+session = Session()
